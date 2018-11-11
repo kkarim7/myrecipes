2764763681d23178e00bb2adf0063c365b8cb4c7
Khaleel Karim <khaleel.karim@gmail.com>
2018-11-11 22:42:13 +0000

    implement edit and delete

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -21,18 +21,20 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
   end
   
-  #test "should get recipes show" do
-  #  get recipe_path(@recipe)
-  #  assert_template 'recipes/show'
-  #  assert_match @recipe.name, response.body
-  #  assert_match @recipe.description, response.body
-  #  assert_match @chef.chefname, response.body
-  #end
+  test "should get recipes show" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_match @recipe.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
+    assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this recipe"
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+  end
   
   test "create new valid recipe" do
     get new_recipe_path
     assert_template 'recipes/new'
-    name_of_recipe = "chicken sautee"
+    name_of_recipe = "Chicken Saute"
     description_of_recipe = "add chicken, add vegetables, cook for 20 minutes, serve delicious meal"
     assert_difference 'Recipe.count', 1 do
       post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe } }
