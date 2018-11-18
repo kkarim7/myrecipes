48447666bd2b4be6768b51fd88f9ce090880ceea
Khaleel Karim <khaleel.karim@gmail.com>
2018-11-18 04:05:52 +0000

    implement chef signup with tests

--- test/integration/recipes_delete_test.rb
+++ test/integration/recipes_delete_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class RecipesDeleteTest < ActionDispatch::IntegrationTest
   
   def setup
-    @chef = Chef.create!(chefname: "Khaleel", email: "khaleel@example.com")
+    @chef = Chef.create!(chefname: "example", email: "example@example.com",
+                        password: "password", password_confirmation: "password")
     @recipe = Recipe.create(name: "Vegetable Saute", description: "Great vegetable sautee, add vegetable and oil", chef: @chef)
   end
   
